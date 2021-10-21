fb3943ae8278725aa5761c57b24e58cf01f4f1a2
Jonathan Gutierrez <jonathan.a.gutierrez9@gmail.com>
2021-10-21 13:38:57 -0300

    manejar rutas invalidas

--- service.js
+++ service.js
@@ -3,6 +3,7 @@ const artist = require("./controller/artistController");
 const album = require("./controller/albumController");
 const track = require("./controller/trackController");
 const errorHandler = require("./controller/errorHandler");
+const errorsAPI = require("./exceptions/apiExeptions");
 
 const app = express();
 
@@ -11,6 +12,9 @@ app.use(express.urlencoded({extended:false}));
 app.use("/api/artists",artist);
 app.use("/api/albums",album);
 app.use("/api/tracks",track);
+app.all("*",(req,res) =>{
+    throw new errorsAPI.InvalidURL();
+});
 app.use(errorHandler);
 
 const PORT = process.env.PORT || 5000;
