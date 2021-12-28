200426e97e4cd761cafad485cb992cb7978ee78b
gguzman89 <ggtpi0gabriel@gmail.com>
2021-12-27 23:37:50 -0300

    Organize function getLyrics
    * update gitignore

--- models/musixmatch.js
+++ models/musixmatch.js
@@ -1,59 +1,49 @@
 let rp = require('request-promise');
+const apiKey = require("../musixmatchCreds.json");
 
 const BASE_URL = 'http://api.musixmatch.com/ws/1.1';
 
 function getIdTrack(namTrack){
-    let options = 
-    {
-        uri: BASE_URL + "/track.search",
+    let options = {
+        uri: `${BASE_URL}/track.search`,
         qs: {
-        apikey: '9f82919921fd949e955479b02b6c26bc',
+        apikey: apiKey.access_token,
         q_track : namTrack,
-        s_track_rating : "desc",
-        
+        s_track_rating : "desc"
     },
-    json: true, // Automatically parses the JSON string in the response
+        json: true
     };
 
-    return rp.get(options)
-    .then((response) =>{
+    return rp.get(options).then( response => {
         const header = response.message.header;
         const body = response.message.body;
         if (header.status_code !== 200){
             throw new Error('status code != 200');
         }
         return body.track_list[0].track.track_id;
-    })
-    .then(id =>{
+    }).then( id => {
         return lyricsForTrack(id);
-    })
-    .catch((error) => {
-        console.log('algo salio mal', error);
-    });
+    }).catch((error) => { console.log(error) });
 }
 
 function lyricsForTrack(idTrack){
-    let options = 
-    {
-        uri: BASE_URL + "/track.lyrics.get",
+    let options = {
+        uri: `${BASE_URL}/track.lyrics.get`,
         qs: {
-        apikey: '9f82919921fd949e955479b02b6c26bc',
+        apikey: apiKey.access_token,
         track_id : idTrack,
     },
-    json: true, // Automatically parses the JSON string in the response
+        json: true
     };
-    return rp.get(options)
-    .then(response => {
+
+    return rp.get(options).then(response => {
         const header = response.message.header;
         const body = response.message.body;
         if (header.status_code !== 200){
             throw new Error('status code != 200');
         }
         return body.lyrics.lyrics_body;
-    })
-    .catch((error) => {
-        console.log('algo salio mal', error);
-    });
+    }).catch((error) => { console.log(error) });
 }
 
 module.exports = getIdTrack;
